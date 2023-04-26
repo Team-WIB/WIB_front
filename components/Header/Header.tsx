b1b32d2e856eb67d8fc8f5f783423eac8f48e249
JYG <jojeayun900@gmail.com>
2023-04-26 19:55:36 +0900

    style Header

---
+++ components/Header/Header.tsx
@@ -0,0 +1,10 @@
+/** @jsxImportSource @emotion/react */
+import * as S from "./style"
+
+export default function Home() {
+  return (
+    <div css={S.Positioner}>
+      <h1 css={S.HeaderText}>WIB</h1>
+    </div>
+  )
+}
---
+++ components/Header/style.ts
@@ -0,0 +1,14 @@
+import { css } from "@emotion/react";
+
+export const Positioner = css`
+    width: 100%;
+    height: 70px;
+    background-color: rgb(51, 51, 51);
+`
+
+export const HeaderText = css`
+    height: 70px;
+    margin: 0px 0px 0px 50px;
+    line-height: 70px;
+    color: white;
+`
