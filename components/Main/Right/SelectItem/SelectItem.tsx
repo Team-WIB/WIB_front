147603a287503247692e19cac2244451df190a14
선채훈 <sunchaehoon@naver.com>
2023-06-20 10:36:35 +0900

    Fix: SelectItem

--- components/Main/Right/SelectItem/SelectItem.tsx
+++ components/Main/Right/SelectItem/SelectItem.tsx
@@ -1,19 +1,70 @@
 /** @jsxImportSource @emotion/react */
-import * as S from "./style"
-
+import { useRecoilState } from "recoil";
+import * as S from "./style";
+import { AddFormAtom, FixFormAtom } from "@/Atom/atoms";
+import CheckIcon from "@/assests/CheckIcon";
+import { useEffect, useState } from "react";
+import CrossIcon from "@/assests/CrossIcon";
+import AddIcon from "@/assests/AddIcon";
+import FixIcon from "@/assests/FixIcon";
 
 export default function SelectItem() {
-    return (
-        <div css={S.Positioner}>
-            <div css={S.Positioner2}>
-                <div css={S.SelectTitle}>
-                    <p>React란 무엇인가요?</p>
-                </div>
-                <div css={S.IconBtn}><img src="/icon_edit.png" /></div>
-            </div>
-            <div>
-                <div css={S.IconBtn}><img src="/icon_plus.png" /></div>
-            </div>
+  const [addForm, setAddForm] = useRecoilState(AddFormAtom);
+  const [fixForm, setFixForm] = useRecoilState(FixFormAtom);
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    if (addForm === false) {
+      setTitle("");
+    }
+  }, [addForm]);
+
+  return (
+    <div css={S.Positioner}>
+      <div css={S.Positioner2}>
+        <div css={S.SelectTitle}>
+          {addForm === true ? (
+            <S.TitleInput
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          ) : fixForm === true ? (
+            <S.TitleInput
+              onChange={(e) => setTitle(e.target.value)}
+            >React란 무엇인가요?</S.TitleInput>
+          ) : (
+            <p>React란 무엇인가요?</p>
+          )}
         </div>
-    )
+        {addForm === false ? (
+          <S.IconBtn
+            className={fixForm ? "check" : "fix"}
+            onClick={() => {
+              setFixForm((pre) => !pre);
+            }}
+          >
+            {fixForm ? <CheckIcon /> : <FixIcon />}
+          </S.IconBtn>
+        ) : (
+          <S.IconBtn className="cross" onClick={() => setAddForm(false)}>
+            <CrossIcon />
+          </S.IconBtn>
+        )}
+      </div>
+      {fixForm ? (
+        <S.IconBtn className="cross" onClick={() => setFixForm(false)}>
+          <CrossIcon />
+        </S.IconBtn>
+      ) : (
+        <S.IconBtn
+          className="check"
+          onClick={() => {
+            setAddForm((pre) => !pre);
+          }}
+        >
+          {addForm ? <CheckIcon /> : <AddIcon />}
+        </S.IconBtn>
+      )}
+    </div>
+  );
 }
--- components/Main/Right/SelectItem/style.ts
+++ components/Main/Right/SelectItem/style.ts
@@ -1,39 +1,65 @@
 import { css } from "@emotion/react";
+import styled from "@emotion/styled";
 
 export const Positioner = css`
-    width: calc(100% - 8vw);
-    height: 3.5vw;
-    margin: 5vh 4vw 5vh 4vw;
-    display: flex;
-    flex-direction: row;
-`
+  width: calc(100% - 8vw);
+  height: 3.5vw;
+  margin: 5vh 4vw 5vh 4vw;
+  display: flex;
+  flex-direction: row;
+  .check {
+    svg {
+      width: 45px;
+      :hover {
+        fill: green;
+        transition-duration: 0.25s;
+      }
+    }
+  }
+  .cross {
+    transform: scale(0.6);
+  }
+  .fix {
+    transform: scale(0.45);
+  }
+`;
 
 export const Positioner2 = css`
-    width: calc(100% - 75px);
-    font-weight: bold;
-    display: flex;
-    flex-direction: row;
-`
+  width: calc(100% - 75px);
+  font-weight: bold;
+  display: flex;
+  flex-direction: row;
+`;
 export const SelectTitle = css`
-    width: calc(100% - 80px);
-    height: calc(3.5vw - 10px);
-    display: flex;
-    align-items: center;
-    padding: 0px 50px 0px 50px;
-    margin: 2.5px; 
-    font-size: calc(5px + 1vw);
-    color: white;
-`
+  width: calc(100% - 80px);
+  height: calc(3.5vw - 10px);
+  display: flex;
+  align-items: center;
+  padding: 0px 50px 0px 50px;
+  margin: 2.5px;
+  font-size: calc(5px + 1vw);
+  color: white;
+`;
 
-export const IconBtn = css`
-    width: 75px;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    img{
-        cursor: pointer;
-        width: 30px;
-        height: 30px;
-    }
-`
+export const TitleInput = styled.textarea`
+  font-size: 18px;
+  padding: 5px 3px;
+  border: none;
+  outline: none;
+  resize: none;
+  width: 100%;
+  border-radius: 5px;
+`;
+
+export const IconBtn = styled.div`
+  width: 75px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  img {
+    width: 30px;
+    height: 30px;
+  }
+`;
