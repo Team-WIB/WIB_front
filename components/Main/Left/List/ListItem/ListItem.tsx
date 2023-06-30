d46aa182fdc0de887b3a0e21d17c425a1ef78ad8
HWANBINYOO <dngh0825@naver.com>
2023-06-30 10:51:54 +0900

    Fix : ListItem 기능구현

--- components/Main/Left/List/ListItem/ListItem.tsx
+++ components/Main/Left/List/ListItem/ListItem.tsx
@@ -4,7 +4,7 @@ import * as S from "./style";
 export default function ListItem({ item }: { item: string }) {
   return (
     <div css={S.ItemBox}>
-      <p>React란 무엇인가요?</p>
+      <p>{item}</p>
     </div>
   );
 }
