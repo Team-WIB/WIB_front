ccdd72b696ede69c8b9c6429a15e95662174ef11
선채훈 <sunchaehoon@naver.com>
2023-07-03 16:11:04 +0900

    Fix: Detail type

--- types/Detail.ts
+++ types/Detail.ts
@@ -1,5 +1,12 @@
-export interface DetailType {
+export interface ListDetailType {
   id: number;
   question: string;
+  tag: string;
+}
+
+
+export interface DetailType {
+  question: string;
+  answer: string;
   tag: string;
 }
