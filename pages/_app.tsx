9e4b8850baca86cf2b78aa51d6b91634ff0ae2c3
HWANBINYOO <dngh0825@naver.com>
2023-07-03 16:52:28 +0900

    Add : SWR 전역설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,23 @@
 import "@/styles/globals.css";
+import { customAxios } from "@/Utils/customAxios";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <SWRConfig
+      value={{
+        fetcher: (url: string) =>
+          customAxios.get(url).then((response) => response.data),
+        revalidateIfStale: true,
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
+      }}
+    >
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </SWRConfig>
   );
 }
