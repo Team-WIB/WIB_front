9ab31603e2b808da15ff66a7e4333a961001481a
선채훈 <sunchaehoon@naver.com>
2023-07-03 16:07:37 +0900

    Add: SVG CrossIcon

--- assests/CrossIcon.tsx
+++ assests/CrossIcon.tsx
@@ -4,14 +4,14 @@ const CrossIcon = (props: React.SVGProps<SVGSVGElement>) => {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="white"
       className="w-6 h-6"
       {...props}
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M6 18L18 6M6 6l12 12"
       />
     </svg>
