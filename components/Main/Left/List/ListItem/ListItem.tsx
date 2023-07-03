355bff78ff0f8feba0b637dcfc22f9e8451df203
선채훈 <sunchaehoon@naver.com>
2023-07-03 16:09:45 +0900

    Fix: ListItem

--- components/Main/Left/List/ListItem/ListItem.tsx
+++ components/Main/Left/List/ListItem/ListItem.tsx
@@ -1,9 +1,11 @@
 /** @jsxImportSource @emotion/react */
+import { useRouter } from "next/router";
 import * as S from "./style";
 
-export default function ListItem({ item }: { item: string }) {
+export default function ListItem({ item, id }: { item: string; id: number }) {
+  const router = useRouter();
   return (
-    <div css={S.ItemBox}>
+    <div css={S.ItemBox} onClick={() => router.push(`/${id}`)}>
       <p>{item}</p>
     </div>
   );
