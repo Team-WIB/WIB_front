3800c46a14cff4a96c639c2cd403b65ef73854e0
선채훈 <sunchaehoon@naver.com>
2023-07-14 10:08:26 +0900

    Fix: Right

--- components/Main/Right/Right.tsx
+++ components/Main/Right/Right.tsx
@@ -4,16 +4,20 @@ import SelectItem from "./SelectItem/SelectItem"
 import SelectWrite from "./SelectWrite/SelectWrite"
 import { useRouter } from "next/router"
 import { useEffect } from "react";
+import { useRecoilState } from "recoil";
+import { QuestCount } from "@/Atom/atoms";
 
 export default function Right() {
   const router = useRouter();
+  const prevId = parseInt(router.asPath.substring(1));
+  const [questCnt, setQuestCnt] = useRecoilState(QuestCount);
 
     return (
       <div css={S.Positioner}>
         <SelectItem />
         <SelectWrite />
-        <S.PointerArrow left="53%" onClick={() => router.push("/" + (parseInt(router.asPath.substring(1)) - 1))}>{"<"}</S.PointerArrow>
-        <S.PointerArrow left="97%" onClick={() => router.push("/" + (parseInt(router.asPath.substring(1)) + 1))}>{">"}</S.PointerArrow>
+        <S.PointerArrow left="53%" onClick={() => router.push("/" + (prevId > 1 ? prevId - 1 : prevId))}>{"<"}</S.PointerArrow>
+        <S.PointerArrow left="97%" onClick={() => router.push("/" + (prevId + 1))}>{">"}</S.PointerArrow>
       </div>
     )
   }
